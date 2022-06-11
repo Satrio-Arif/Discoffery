60cf925311974eee22f529b684001c84ef4b1042
IlhamSetyoN195150400111037 <79489906+IlhamSetyoN195150400111037@users.noreply.github.com>
2022-06-11 22:19:38 +0700

    Update deploy.sh

--- General-Article-Coffee-API/deploy.sh
+++ General-Article-Coffee-API/deploy.sh
@@ -3,7 +3,7 @@ gcloud builds submit --tag gcr.io/discoffery-352123/discoffery-service
 gcloud run deploy discoffery-service \
   --image gcr.io/discoffery-352123/discoffery-service \
   --add-cloudsql-instances discoffery-352123:asia-southeast2:discofferycoffe \
-  --update-env-vars INSTANCE_CONNECTION_NAME=discoffery-352123:asia-southeast2:discofferycoffe,DB_PASS=discoffery,DB_USER=rootR,DB_NAME=Discoffery \
+  --update-env-vars INSTANCE_CONNECTION_NAME=discoffery-352123:asia-southeast2:discofferycoffe,DB_PASS=discoffery,DB_USER=root,DB_NAME=Discoffery \
   --platform managed \
   --region asia-southeast2 \
   --allow-unauthenticated \
